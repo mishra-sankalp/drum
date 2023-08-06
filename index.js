7474b70fe13df923d877b2070b7ba7d32c3753a8
Sankalp mishra <100511905+mishra-sankalp@users.noreply.github.com>
2023-08-07 00:00:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ for(var i=0;i<document.querySelectorAll(".drum").length;i++)
 function makeSound(buttonsound)
 {
   switch (buttonsound) {
-    case "w":
+    case "m":
           var tom1=new Audio("sounds/tom-1.mp3");
           tom1.play();
       break;
-    case "a":
+    case "u":
     var tom2=new Audio("sounds/tom-2.mp3");
     tom2.play();
     break;
@@ -27,15 +27,15 @@ function makeSound(buttonsound)
           var tom3=new Audio("sounds/tom-3.mp3");
           tom3.play();
       break;
-      case "d":
+      case "i":
             var tom4=new Audio("sounds/tom-4.mp3");
             tom4.play();
         break;
-        case "j":
+        case "c":
               var snare=new Audio("sounds/snare.mp3");
               snare.play();
           break;
-          case "k":
+          case "a":
                var crash=new Audio("sounds/crash.mp3");
                crash.play();
             break;
